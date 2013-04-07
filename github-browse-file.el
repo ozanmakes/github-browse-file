a0823bb0432c8a1caa4ebf78861767fd9b38be29
Ozan Sener <ozan@ozansener.com>
2013-04-07 16:46:53 +0300

    Move “blame” viewing to it's own command

--- github-browse-file.el
+++ github-browse-file.el
@@ -4,7 +4,7 @@
 
 ;; Author: Ozan Sener <ozan@ozansener.com>
 ;; Homepage: https://github.com/osener/github-browse-file
-;; Version: 0.1
+;; Version: 0.2
 ;; Keywords: convenience vc git github
 
 ;;; Installation:
@@ -12,9 +12,10 @@
 ;; Available as a package in Marmalade at http://marmalade-repo.org/
 ;; M-x package-install github-browse-file
 
-;; Usage:
-;;    Call `github-browse-file' to view current file on GitHub. With a prefix
-;;    argument (C-u) "blame" is shown instead of "blob".
+;;; Usage:
+;; Call `github-browse-file' (for the git blob) or `github-browse-file-blame'
+;; (for the git blame) to view current file on GitHub. With a prefix argument
+;; (C-u), you can force them to use the "master" branch.
 
 ;; This file is NOT part of GNU Emacs.
 
@@ -37,8 +38,12 @@
 
 (require 'vc-git)
 
-(defvar github-browse-file-view-blame nil
+(defvar github-browse-file--view-blame nil
   "If non-nil, view \"blame\" instead of \"blob\".
+This should only ever be `let'-bound, not set outright.")
+
+(defvar github-browse-file--force-master nil
+  "Whether to use \"master\" regardless of current branch
 This should only ever be `let'-bound, not set outright.")
 
 (defun github-browse-file--relative-url ()
@@ -63,19 +68,21 @@ Error out if this isn't a GitHub repo."
          (string-equal (substring rev 0 1) ">"))))
 
 (defun github-browse-file--current-rev ()
-  "Return the SHA1 of HEAD if it is not ahead of origin/master. Otherwise,
-return the name of the current branch."
-  (if (github-browse-file--ahead-p)
-      (car (vc-git-branches))
-    (let ((rev (vc-git--run-command-string nil "rev-parse" "HEAD")))
-      (and rev (replace-regexp-in-string "\n" "" rev)))))
+  "Return the SHA1 of HEAD if it is not ahead of origin/master.
+If github-browse-file--force-master is non-nil, return \"master\".
+Otherwise, return the name of the current  branch."
+  (cond
+   (github-browse-file--force-master "master")
+   ((github-browse-file--ahead-p) (car (vc-git-branches)))
+   (t (let ((rev (vc-git--run-command-string nil "rev-parse" "HEAD")))
+        (and rev (replace-regexp-in-string "\n" "" rev))))))
 
 (defun github-browse-file--browse-url (&optional anchor)
   "Load http://github.com/user/repo/file#ANCHOR in a web browser and add it to
 the kill ring."
   (let ((url (concat "https://github.com/"
                      (github-browse-file--relative-url) "/"
-                     (if github-browse-file-view-blame "blame" "blob") "/"
+                     (if github-browse-file--view-blame "blame" "blob") "/"
                      (github-browse-file--current-rev) "/"
                      (github-browse-file--repo-relative-path)
                      (when anchor (concat "#" anchor)))))
@@ -83,15 +90,16 @@ the kill ring."
     (browse-url url)))
 
 ;;;###autoload
-(defun github-browse-file (view-blame)
+(defun github-browse-file (force-master)
   "Show the GitHub webpage for the current file. The URL for the webpage is
-added to the kill ring.
+added to the kill ring. With a prefix argument, \"master\" is used
+regardless of the current branch.
 
 In Transient Mark mode, if the mark is active, highlight the contents of the
 region."
   (interactive "P")
   (let ((path (github-browse-file--repo-relative-path))
-        (github-browse-file-view-blame view-blame)
+        (github-browse-file--force-master force-master)
         start
         end)
     (when mark-ring
@@ -104,5 +112,17 @@ region."
        (if (eq start end)
            (format "L%d" start)
          (format "L%d-%d" start end))))))
+
+;;;###autoload
+(defun github-browse-file-blame (force-master)
+  "Show the GitHub blame page for the current file. The URL for the webpage is
+added to the kill ring. With a prefix argument, \"master\" is used
+regardless of the current branch.
+
+In Transient Mark mode, if the mark is active, highlight the contents of the
+region."
+  (interactive "P")
+  (let ((github-browse-file--view-blame t))
+    (github-browse-file force-master)))
 
 ;;; github-browse-file.el ends here
